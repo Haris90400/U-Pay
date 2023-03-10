7cbc86534656fd121a49683e8f6228297db1e67a
Khan Haris <haris90400@gmail.com>
2023-03-11 00:59:18 +0530

    Updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -59,138 +59,148 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xff24B3A8),
-              Color(0xff24B3A8),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: 60.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Hero(
-                      tag: 'logo',
-                      child: Image.asset(
-                        'images/upayy.png',
-                        height: 120.0,
-                      ),
-                    ),
-                  ],
-                ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xff24B3A8),
+                  Color(0xff24B3A8),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'LOGIN',
-                      style: TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w900,
+            ),
+            child: Container(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 60.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Hero(
+                          tag: 'logo',
+                          child: Image.asset(
+                            'images/upayy.png',
+                            height: 120.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(50.0, 20.0, 50.0, 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'LOGIN',
+                          style: TextStyle(
+                              fontSize: 35.0,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white,
+                              decoration: TextDecoration.none),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        CircleAvatar(
+                          child: Icon(
+                            Icons.login,
+                            size: 35.0,
+                            color: Colors.black,
+                          ),
+                          backgroundColor: Colors.white,
+                          radius: 30.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.symmetric(horizontal: 20.0),
+                        decoration: BoxDecoration(
                           color: Colors.white,
-                          decoration: TextDecoration.none),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    CircleAvatar(
-                      child: Icon(
-                        Icons.login,
-                        size: 35.0,
-                        color: Colors.black,
-                      ),
-                      backgroundColor: Colors.white,
-                      radius: 30.0,
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20.0),
-                  child: Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20.0),
-                        topLeft: Radius.circular(20.0),
-                      ),
-                    ),
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(15.0, 35.0, 15.0, 0.0),
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20.0),
+                            topLeft: Radius.circular(20.0),
+                          ),
+                        ),
                         child: SingleChildScrollView(
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: 35.0,
+                          child: Padding(
+                            padding: EdgeInsets.fromLTRB(15.0, 35.0, 15.0, 0.0),
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    height: 35.0,
+                                  ),
+                                  TextField(
+                                    controller: emailController,
+                                    decoration: kInputFieldDecoration.copyWith(
+                                      hintText: 'Enter your email',
+                                      prefixIcon: Icon(Icons.email),
+                                    ),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        print(value);
+                                      });
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height: 30.0,
+                                  ),
+                                  PasswordField(
+                                    controller: passwordController,
+                                    prefixIconData: Icons.visibility,
+                                    Name: 'Enter your password',
+                                    prefixIconColor: Colors.grey,
+                                    onChanged: (value) {
+                                      print(value);
+                                    },
+                                  ),
+                                  SizedBox(height: 16.0),
+                                  Text(
+                                    _errorMessage,
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  SizedBox(
+                                    height: 200.0,
+                                  ),
+                                  RoundedButton(
+                                      Colour: Color(0xff24B3A8),
+                                      Name: 'Log In',
+                                      onPressed: () {
+                                        logIn();
+                                        // Navigator.push(
+                                        //     context,
+                                        //     MaterialPageRoute(
+                                        //         builder: (context) => Home()));
+                                      })
+                                ],
                               ),
-                              TextField(
-                                controller: emailController,
-                                decoration: kInputFieldDecoration.copyWith(
-                                  hintText: 'Enter your email',
-                                  prefixIcon: Icon(Icons.email),
-                                ),
-                                onChanged: (value) {
-                                  setState(() {
-                                    print(value);
-                                  });
-                                },
-                              ),
-                              SizedBox(
-                                height: 30.0,
-                              ),
-                              PasswordField(
-                                controller: passwordController,
-                                prefixIconData: Icons.visibility,
-                                Name: 'Enter your password',
-                                prefixIconColor: Colors.grey,
-                                onChanged: (value) {
-                                  print(value);
-                                },
-                              ),
-                              SizedBox(height: 16.0),
-                              Text(
-                                _errorMessage,
-                                style: TextStyle(color: Colors.red),
-                              ),
-                              SizedBox(
-                                height: 200.0,
-                              ),
-                              RoundedButton(
-                                  Colour: Color(0xff24B3A8),
-                                  Name: 'Log In',
-                                  onPressed: () {
-                                    logIn();
-                                    // Navigator.push(
-                                    //     context,
-                                    //     MaterialPageRoute(
-                                    //         builder: (context) => Home()));
-                                  })
-                            ],
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
+          if (_isLoading) // Show the CircularProgressIndicator if loading
+            Positioned.fill(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
       ),
     );
   }
