205afb7071ef413ab3a9c917dc539979fef45fd7
Khan Haris <haris90400@gmail.com>
2023-03-13 23:05:31 +0530

    added transaction facility

--- lib/components/upi_widget.dart
+++ lib/components/upi_widget.dart
@@ -17,6 +17,7 @@ class otpWidget extends StatefulWidget {
 }
 
 class _otpWidgetState extends State<otpWidget> {
+  bool _isLoading = false;
   //Function to get the logged in user U-PAY Pin
   Future<String> getUpayPin(String uid) async {
     final CollectionReference users =
@@ -111,6 +112,7 @@ class _otpWidgetState extends State<otpWidget> {
         });
       }
     }
+
     // Update current user's balance in Firestore
     await FirebaseFirestore.instance
         .collection('Users')
@@ -159,98 +161,110 @@ class _otpWidgetState extends State<otpWidget> {
           backgroundColor: Color(0xff24B3A8),
           centerTitle: true,
         ),
-        body: Padding(
-          padding: EdgeInsets.only(top: 80.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'Enter 6 digit U-PAY Pin',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Text(
-                error_message,
-                style: TextStyle(color: Colors.red),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: List.generate(6, (index) {
-                      return Container(
-                        width: 40.0,
-                        height: 35.0,
-                        decoration: BoxDecoration(),
-                        child: TextField(
-                          style: TextStyle(fontSize: 20.0),
-                          textAlign: TextAlign.center,
-                          obscureText: _obscureText,
-                          focusNode: focusNodes[index],
-                          controller: controllers[index],
-                          decoration: InputDecoration(
-                            focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.black, width: 1),
+        body: Stack(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 80.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    'Enter 6 digit U-PAY Pin',
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    error_message,
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: List.generate(6, (index) {
+                          return Container(
+                            width: 40.0,
+                            height: 35.0,
+                            decoration: BoxDecoration(),
+                            child: TextField(
+                              style: TextStyle(fontSize: 20.0),
+                              textAlign: TextAlign.center,
+                              obscureText: _obscureText,
+                              focusNode: focusNodes[index],
+                              controller: controllers[index],
+                              decoration: InputDecoration(
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.black, width: 1),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.grey, width: 1),
+                                ),
+                                counterText: '',
+                              ),
+                              //textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              maxLength: 2,
+                              onChanged: (value) {
+                                if (value.length == 1) {
+                                  focusNodes[index].unfocus();
+                                  if (index != 5) {
+                                    FocusScope.of(context).requestFocus(
+                                      focusNodes[index + 1],
+                                    );
+                                  }
+                                }
+                              },
                             ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.grey, width: 1),
-                            ),
-                            counterText: '',
-                          ),
-                          //textAlign: TextAlign.center,
-                          keyboardType: TextInputType.number,
-                          maxLength: 2,
-                          onChanged: (value) {
-                            if (value.length == 1) {
-                              focusNodes[index].unfocus();
-                              if (index != 5) {
-                                FocusScope.of(context).requestFocus(
-                                  focusNodes[index + 1],
-                                );
-                              }
-                            }
-                          },
-                        ),
-                      );
-                    })),
+                          );
+                        })),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  GestureDetector(
+                    child: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility),
+                    onTap: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff24B3A8)),
+                      child: Text('Submit'),
+                      onPressed: () async {
+                        String pin = '';
+                        controllers.forEach((element) => pin += element.text);
+                        bool isVerified = await verifyUpayPin();
+                        if (isVerified) {
+                          _isLoading = true;
+                          updateBalance();
+                        }
+                      },
+                    ),
+                  )
+                ],
               ),
-              SizedBox(
-                height: 10.0,
-              ),
-              GestureDetector(
-                child: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility),
-                onTap: () {
-                  setState(() {
-                    _obscureText = !_obscureText;
-                  });
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 30.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff24B3A8)),
-                  child: Text('Submit'),
-                  onPressed: () async {
-                    String pin = '';
-                    controllers.forEach((element) => pin += element.text);
-                    bool isVerified = await verifyUpayPin();
-                    if (isVerified) {
-                      updateBalance();
-                    }
-                  },
+            ),
+            if (_isLoading) // Show the CircularProgressIndicator if loading
+              Positioned.fill(
+                child: Center(
+                  child: CircularProgressIndicator(),
                 ),
-              )
-            ],
-          ),
+              ),
+          ],
         ),
       ),
     );
